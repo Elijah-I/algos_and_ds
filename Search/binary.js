79c343433f4d4aaba3a781cc7f2385894c01924e
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-01-29 12:23:51 +0300

    ref: Search -> binary, add 2 versions: recursive, cycle

--- Search/binary.js
+++ Search/binary.js
@@ -1,9 +1,9 @@
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const binarySearch = (array, item, offset = 0) => {
-  let middle = Math.ceil(array.length / 2);
-  let left = array.slice(0, middle);
-  let right = array.slice(middle);
+const binarySearchRecursion = (array, item, offset = 0) => {
+  const middle = Math.ceil(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
 
   if (array[middle] === item) {
     return middle + offset;
@@ -18,6 +18,39 @@ const binarySearch = (array, item, offset = 0) => {
   }
 };
 
-const index = binarySearch(sortedArray, 3);
+const binarySearchCycle = (array, item) => {
+  const recalculateSlices = () => {
+    middle = Math.ceil(array.length / 2);
+    left = array.slice(0, middle);
+    right = array.slice(middle);
+  };
+
+  let offset = 0;
+  let middle, left, right;
+
+  recalculateSlices();
+
+  while (array.length > 1) {
+    if (array[middle] === item) {
+      return middle + offset;
+    }
+
+    if (array[middle] < item) {
+      offset += left.length;
+
+      array = array.slice(middle);
+      recalculateSlices();
+    }
+
+    if (array[middle] > item) {
+      array = array.slice(0, middle);
+      recalculateSlices();
+    }
+  }
+
+  return null;
+};
+
+const index = binarySearchCycle(sortedArray, 7);
 
 console.log(index, sortedArray[index]);
