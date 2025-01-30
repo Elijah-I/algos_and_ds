d7f6492484807900c39f01db92fa73d359fb6a3a
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-01-30 14:30:06 +0300

    ref: breadthSearch logic

--- Graph/breadthSearch.js
+++ Graph/breadthSearch.js
@@ -3,7 +3,8 @@ graph.a = ["b", "c"];
 graph.b = ["f"];
 graph.c = ["d", "e"];
 graph.d = ["f"];
-graph.e = ["f"];
+graph.e = ["f", "x"];
+graph.x = ["g"];
 graph.f = ["g"];
 
 const isBreadthSearch = (graph, start, end) => {
@@ -27,35 +28,23 @@ const isBreadthSearch = (graph, start, end) => {
   return false;
 };
 
-const breadthSearch = (graph, current, end, total = [], thread = 0) => {
-  const passed = [];
+const breadthSearch = (graph, current, end, prefix = "", paths = [[]]) => {
+  if (current === end) {
+    paths[paths.length - 1] = (prefix + current).split("");
+    paths.push([]);
 
-  if (!graph[current] || current === end) {
     return;
   }
 
-  while (passed.length !== graph[current].length) {
-    const currentIndex = passed.length;
-    const next = graph[current][currentIndex];
-
-    if (!total[thread]) {
-      let prefix = [current];
-      const prevThread = total[thread - 1];
-
-      if (prevThread) {
-        const prefixIndex = prevThread.findIndex((value) => value === current);
-        prefix = prevThread.slice(0, prefixIndex + 1);
-      }
-
-      total[thread] = prefix;
-    }
-
-    total[thread].push(next);
-
-    passed.push(breadthSearch(graph, next, end, total, thread++));
+  if (!graph[current]) {
+    return;
   }
 
-  return total.filter((thread) => end === thread[thread.length - 1]);
+  for (let i = 0; i < graph[current].length; i++) {
+    breadthSearch(graph, graph[current][i], end, prefix + current, paths);
+  }
+
+  return paths.slice(0, paths.length - 1);
 };
 
 const shortestPath = breadthSearch(graph, "a", "g");
