d47002227bddf5694aaa9c008c4142d7e4b5ddcb
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-01-31 09:26:25 +0300

    feat: Math -> coutDigits

---
+++ Math/countDigits.ts
@@ -0,0 +1,8 @@
+const countDigits = (num: number): number =>
+  String(num)
+    .split("")
+    .reduce((total, digit) => (total += Number(num % Number(digit) === 0)), 0);
+
+const total = countDigits(1248);
+
+console.log(total);
