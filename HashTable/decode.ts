88db29e3d17f2f153239656e19bcc007573e859e
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-03 09:30:04 +0300

    feat: HashTable -> decode

---
+++ HashTable/decode.ts
@@ -0,0 +1,18 @@
+const decodeMessage = (key: string, message: string): string => {
+  const alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+  const uniqKeys = Array.from(new Set(key.replaceAll(" ", "")));
+
+  return message
+    .split("")
+    .map((char) =>
+      char === " " ? char : alphabet[uniqKeys.indexOf(char) || 0]
+    )
+    .join("");
+};
+
+const message = decodeMessage(
+  "the quick brown fox jumps over the lazy dog",
+  "vkbs bs t suepuv"
+);
+
+console.log(message);
