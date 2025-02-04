06f482897cf2532dededa9fd0d3d19af7e3e8705
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-04 17:59:14 +0300

    feat: TypeScript -> updateEntity

---
+++ TypeScript/updateEntity.ts
@@ -0,0 +1,24 @@
+interface User {
+  id: number;
+  name: string;
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+const user: User = { id: 1, name: "user" };
+const updatedUser = updateEntity(user, { name: "Bob" });
+
+const post: Post = { id: 1, title: "post" };
+const updatedPost = updateEntity(post, { title: "myPost" });
+
+updateEntity(user, { email: "user@mail.ru" });
+
+function updateEntity<Entity extends object>(
+  entity: Entity,
+  fields: { [Key in keyof Entity]?: Entity[Key] }
+): Entity {
+  return Object.assign(entity, fields);
+}
