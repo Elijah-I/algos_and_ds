937c6c5a561fea2c8413e54b112a44dc4d799144
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-04 18:42:48 +0300

    ref: MyPick

--- TypeScript/myPick.ts
+++ TypeScript/myPick.ts
@@ -5,7 +5,7 @@ interface User {
 }
 
 type MyPick<Type, IncludeKeys extends keyof Type> = {
-  [Key in keyof Type as Key extends IncludeKeys ? Key : never]: Type[Key];
+  [Key in IncludeKeys]: Type[Key];
 };
 
 const user: MyPick<User, "id"> = {
