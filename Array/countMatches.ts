9001172cf4094c818683c29e097280bb679cfae9
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-10 20:14:24 +0300

    feat: Array -> countMatches

---
+++ Array/countMatches.ts
@@ -0,0 +1,26 @@
+const items = [
+  ["phone", "blue", "pixel"],
+  ["computer", "silver", "lenovo"],
+  ["phone", "gold", "iphone"]
+];
+const ruleKey = "color";
+const ruleValue = "silver";
+
+const countMatches = <T extends "type" | "color" | "name">(
+  items: string[][],
+  ruleKey: T,
+  ruleValue: string
+): number => {
+  const bindKey = { type: 0, color: 1, name: 2 };
+
+  return items.reduce((total, item) => {
+    if (item[bindKey[ruleKey]] === ruleValue) {
+      total++;
+    }
+    return total;
+  }, 0);
+};
+
+const total = countMatches(items, ruleKey, ruleValue);
+
+console.log(total);
