ead67a68cf36ff6929c2e03b98e2d3e60eacb7b7
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-17 11:39:35 +0300

    feat: Search -> findMin

---
+++ Search/findMin.ts
@@ -0,0 +1,36 @@
+const numbers = [2, 1];
+
+const findMin = (numbers: number[]): number => {
+  let start = 0;
+  let end = numbers.length - 1;
+
+  while (start !== end) {
+    const pivot = Math.floor((start + end) / 2);
+
+    if (numbers[pivot + 1] < numbers[pivot]) {
+      if (numbers[pivot + 1] === undefined) {
+        return numbers[pivot];
+      }
+      return numbers[pivot + 1];
+    }
+
+    if (
+      numbers[pivot - 1] === undefined ||
+      numbers[pivot - 1] > numbers[pivot]
+    ) {
+      return numbers[pivot];
+    }
+
+    if (numbers[pivot] < numbers[end]) {
+      end = pivot;
+    } else {
+      start = pivot + 1;
+    }
+  }
+
+  return numbers[0];
+};
+
+const min = findMin(numbers);
+
+console.log(min);
