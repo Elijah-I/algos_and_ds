8b00438c5a461b1e2d64d43ce05b2739a031ac51
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-02-25 12:27:19 +0300

    feat: Array -> findNumbers

---
+++ Array/findNumbers.ts
@@ -0,0 +1,12 @@
+const numbers = [12, 345, 2, 6, 7896];
+
+const findNumbers = (numbers: number[]): number => {
+  return numbers.reduce(
+    (total, number) => (String(number).length % 2 ? total : ++total),
+    0
+  );
+};
+
+const found = findNumbers(numbers);
+
+console.log(found);
