559649d61918be1014e13641a8d28fc8390712bb
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-04-17 16:25:09 +0300

    feat: Stack -> isBalancedExpression

---
+++ Stack/isBalancedExpression.ts
@@ -0,0 +1,37 @@
+const expressionA = "{(([a] + <b>))[c]}";
+const expressionB = "(([a] + <b>)[c]";
+
+const isBalancedExpression = (expression: string): boolean => {
+  const stack: string[] = [];
+  const openBrackets = ["(", "[", "<", "{"];
+  const closeBrackets = [")", "]", ">", "}"];
+
+  for (const char of expression) {
+    const openType = openBrackets.findIndex((bracket) => bracket === char);
+    const closeType = closeBrackets.findIndex((bracket) => bracket === char);
+
+    if (openType !== -1) {
+      stack.push(char);
+      continue;
+    }
+
+    if (closeType !== -1) {
+      const lastOpenType = openBrackets.findIndex(
+        (bracket) => bracket === stack[stack.length - 1]
+      );
+
+      if (lastOpenType !== closeType) {
+        return false;
+      }
+
+      stack.pop();
+    }
+  }
+
+  return !stack.length;
+};
+
+const isBalancedA = isBalancedExpression(expressionA);
+const isBalancedB = isBalancedExpression(expressionB);
+
+console.log(isBalancedA, isBalancedB);
