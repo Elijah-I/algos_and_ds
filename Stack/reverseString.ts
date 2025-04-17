00617afb202fe31ff6f6ec3b6fca94e51d48e44d
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-04-17 09:20:46 +0300

    feat: Stack -> reverseString

---
+++ Stack/reverseString.ts
@@ -0,0 +1,19 @@
+const s = "abcd";
+
+const reverseString = (s: string) => {
+  let reversedString = "";
+
+  const stack = [];
+  for (const char of s) {
+    stack.push(char);
+  }
+  while (stack.length) {
+    reversedString += stack.pop();
+  }
+
+  return reversedString;
+};
+
+const reversed = reverseString(s);
+
+console.log(reversed);
