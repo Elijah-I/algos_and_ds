49f8f69d6b671319c3630d9cc47a665ccf8d798d
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-04-28 11:36:50 +0300

    feat: Queue -> priorityQueue

---
+++ Queue/priorityQueue.ts
@@ -0,0 +1,30 @@
+class priorityQueue {
+  private size: number = 0;
+  private queue: number[] = [];
+
+  insert(item: number) {
+    for (let i = this.size; i >= 0; i--) {
+      if (this.queue[i - 1] > item) {
+        this.queue[i] = this.queue[i - 1];
+      } else {
+        this.queue[i] = item;
+        break;
+      }
+    }
+
+    this.size++;
+  }
+
+  print() {
+    console.log(this.queue);
+  }
+}
+
+const queue = new priorityQueue();
+
+queue.insert(4);
+queue.insert(1);
+queue.insert(5);
+queue.insert(7);
+queue.insert(2);
+queue.print();
