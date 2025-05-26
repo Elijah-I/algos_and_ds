2a11332593ef193bf0a4994dd7ca7cb123b3e355
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2025-05-26 12:33:25 +0300

    feat: Array -> moveZeroes

---
+++ Array/moveZeroes.ts
@@ -0,0 +1,33 @@
+const numbers = [0, 0, 1, 0, 3, 12];
+
+const moveZeroes = (numbers: number[]): number[] => {
+  let stopPosition = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    const stopIndex = numbers.length - stopPosition - 1;
+
+    if (i >= stopIndex) {
+      return numbers;
+    }
+
+    if (numbers[i] === 0) {
+      stopPosition++;
+
+      for (let j = i + 1; j <= stopIndex; j++) {
+        numbers[j - 1] = numbers[j];
+      }
+
+      numbers[stopIndex] = 0;
+
+      if (numbers[i] === 0) {
+        i--;
+      }
+    }
+  }
+
+  return numbers;
+};
+
+const moved = moveZeroes(numbers);
+
+console.log(moved);
